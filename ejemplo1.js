1efc1f30593d7cfaebed78b5794a2f620859d791
EscobarLeny <lennyescobar1993@gmail.com>
2022-06-24 21:43:49 -0500

    tercer commit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,14 +1,22 @@
+
+
+const _private=new WeakMap();
 class Libros{
     constructor(autor,nombre,numerodepaginas){
-    this.autor=autor
-    this.nombre=nombre
-    this.numerodepaginas=numerodepaginas
+    const propiedades={ 
+    _autor:autor,
+    _nombre:nombre,
+    _numerodepaginas:numerodepaginas
     }
+    _private.set(this,{propiedades})
+}
     set nombrelibro(nombre1){
+        return _private.get(this).propiedades['_nombre']=nombre1
 
     }
     get mostrarlibro(){
-        
+        return _private.get(this).propiedades['-nombre'];
+
     }
 }
  let libro1 =new Libros("Gabriel Garcia Marquez","cien años de soledad","350")
