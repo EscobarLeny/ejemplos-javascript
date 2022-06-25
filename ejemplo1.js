c977d35ed9be4f54a058b5f203dca3105bb159b9
EscobarLeny <lennyescobar1993@gmail.com>
2022-06-24 20:34:21 -0500

    segundo commit

--- ejemplo1.js
+++ ejemplo1.js
@@ -1,3 +1,15 @@
 class Libros{
-    
+    constructor(autor,nombre,numerodepaginas){
+    this.autor=autor
+    this.nombre=nombre
+    this.numerodepaginas=numerodepaginas
+    }
+    set nombrelibro(nombre1){
+
+    }
+    get mostrarlibro(){
+        
+    }
 }
+ let libro1 =new Libros("Gabriel Garcia Marquez","cien años de soledad","350")
+ console.log(libro1)
